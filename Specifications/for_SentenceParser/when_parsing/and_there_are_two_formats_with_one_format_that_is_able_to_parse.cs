cda9e84c1772a873c09627a72761882e44275816
Einar Ingebrigtsen <einar@dolittle.com>
2019-07-02 08:34:17 +0200

    Fixing specs

--- Specifications/for_SentenceParser/when_parsing/and_there_are_two_formats_with_one_format_that_is_able_to_parse.cs
+++ Specifications/for_SentenceParser/when_parsing/and_there_are_two_formats_with_one_format_that_is_able_to_parse.cs
@@ -2,19 +2,24 @@
  *  Copyright (c) Dolittle. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+using System.Linq;
+using System.Collections.Generic;
 using Machine.Specifications;
 
 namespace Dolittle.TimeSeries.NMEA.for_SentenceParser.when_parsing
 {
     public class and_there_are_two_formats_with_one_format_that_is_able_to_parse : given.two_formats
     {
+        const string sub_type = "Sub type";
         const string exepected_result = "Fourty Two";
-        static object result;
+        static IEnumerable<ParsedResult> result;
 
-        Establish context = () => second_format.Setup(_ => _.Parse(second_format_values)).Returns(exepected_result);
+        Establish context = () => second_format.Setup(_ => _.Parse(second_format_values)).Returns(new[] { new ParsedResult(sub_type,exepected_result) });
+       
 
         Because of = () => result = sentence_parser.Parse(second_format_sample);
 
-        It should_return_the_expected_result = () => result.ShouldEqual(exepected_result);
+        It should_return_the_expected_sub_type = () => result.First().Type.ShouldEqual(sub_type);
+        It should_return_the_expected_result = () => result.First().Result.ShouldEqual(exepected_result);
     }
 }
--- Specifications/for_SentenceParser/when_parsing/and_there_is_one_format_that_is_able_to_parse.cs
+++ Specifications/for_SentenceParser/when_parsing/and_there_is_one_format_that_is_able_to_parse.cs
@@ -2,23 +2,26 @@
  *  Copyright (c) Dolittle. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+using System.Linq;
+using System.Collections.Generic;
 using Machine.Specifications;
 
 namespace Dolittle.TimeSeries.NMEA.for_SentenceParser.when_parsing
 {
     public class and_there_is_one_format_that_is_able_to_parse : given.one_format
     {
+        const string sub_type = "Sub type";
         const string exepected_result = "Fourty Two";
-        static object result;
+        static IEnumerable<ParsedResult> result;
 
-        Establish context = () => format.Setup(_ => _.Parse(values)).Returns(exepected_result);
+        Establish context = () => format.Setup(_ => _.Parse(values)).Returns(new[] { new ParsedResult(sub_type,exepected_result) });
 
         Because of = () => 
         {
             result = sentence_parser.Parse(valid_format);
         };
 
-
-        It should_return_the_expected_result = () => result.ShouldEqual(exepected_result);
+        It should_return_the_expected_sub_type = () => result.First().Type.ShouldEqual(sub_type);
+        It should_return_the_expected_result = () => result.First().Result.ShouldEqual(exepected_result);
     }
 }
