0976c2f34210559937e3c97b73c46077189613c9
sigbjorn <sigbjorn.rudaa@gmail.com>
2019-07-02 16:17:33 +0200

    Heading – True parser

---
+++ Specifications/for_SentenceFormats/given/a_HEHDT_parser.cs
@@ -0,0 +1,20 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+using System.Linq;
+using Dolittle.TimeSeries.NMEA.SentenceFormats;
+using Machine.Specifications;
+
+namespace Dolittle.TimeSeries.NMEA.for_SentenceFormats.given
+{
+    public class a_HEHDT_parser : parsed_results
+    {
+        protected static HEHDT parser;
+        
+        Establish context = () =>
+        {
+            parser = new HEHDT();
+        };
+    }
+}
